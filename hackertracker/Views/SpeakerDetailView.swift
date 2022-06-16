ca8327b5c8b2fda83a6d40400b1ff889a45fea86
cak <cak@fastmail.com>
2022-06-16 06:36:16 -0400

    update speaker detail view

--- hackertracker/Views/SpeakerDetailView.swift
+++ hackertracker/Views/SpeakerDetailView.swift
@@ -12,14 +12,38 @@ struct SpeakerDetailView: View {
     var id: Int = 1
     var speaker: Speaker?
     var body: some View {
-        VStack {
-            Text(viewModel.speaker.name)
-            Text(viewModel.speaker.title ?? "Hacker")
-            Divider()
-            Text(viewModel.speaker.description)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text(viewModel.speaker.name).font(.largeTitle)
+                Text(viewModel.speaker.title ?? "Hacker")
+                Divider()
+                Text(viewModel.speaker.description).padding(.top).padding()
+                Text("Events").font(.headline).padding(.top)
+                VStack(alignment: .leading) {
+                    ForEach(speaker?.events ?? []) { event in
+                        SpeakerEventsView(event: event, bookmarks: [])
+                    }
+                }
+                .rectangleBackground()
+            }
+            Spacer()
         }
         .onAppear {
             viewModel.fetchData(speakerId: String(id))
+        }
+    }
+}
+
+struct SpeakerEventsView: View {
+    var event: SpeakerEvent
+    @State var bookmarks: [Int]
+
+    var body: some View {
+        HStack {
+            Rectangle().fill(Color.yellow).frame(width: 10, height: .infinity)
+            VStack(alignment: .leading) {
+                Text(event.title ?? "").fontWeight(.bold)
+            }
         }
     }
 }
@@ -29,12 +53,12 @@ struct SpeakerDetailView_Previews: PreviewProvider {
         let preview_speaker = Speaker(docId: nil,
                                       id: 123,
                                       conferenceName: "DEFCON30",
-                                      description: "Just as short test description",
+                                      description: "Just as short test description. ",
                                       link: "https://google.com/",
                                       name: "Speaker Name",
                                       title: "Chief Hacking Officer",
                                       twitter: "defcon",
-                                      events: [])
-        SpeakerDetailView(id: 123, speaker: preview_speaker)
+                                      events: [SpeakerEvent(id: 1337, title: "Speaker event1 title"), SpeakerEvent(id: 1338, title: "Speaker event2 title")])
+        SpeakerDetailView(id: 123, speaker: preview_speaker).preferredColorScheme(.dark)
     }
 }
