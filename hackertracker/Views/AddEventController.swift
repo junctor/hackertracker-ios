1fd96e22d590c1f855f35b95571c8cef0455f87a
cak <cak@fastmail.com>
2024-04-03 05:32:47 -0400

    Linted and formatted with apple/swift-format

--- hackertracker/Views/AddEventController.swift
+++ hackertracker/Views/AddEventController.swift
@@ -10,74 +10,78 @@ import EventKitUI
 import UIKit
 
 class AddEventController: UIViewController, EKEventEditViewDelegate {
-    let eventStore = EKEventStore()
-    var event: Event?
+  let eventStore = EKEventStore()
+  var event: Event?
 
-    func eventEditViewController(_ controller: EKEventEditViewController, didCompleteWith _: EKEventEditViewAction) {
-        controller.dismiss(animated: true, completion: nil)
-        parent?.dismiss(animated: true, completion: nil)
-    }
+  func eventEditViewController(
+    _ controller: EKEventEditViewController, didCompleteWith _: EKEventEditViewAction
+  ) {
+    controller.dismiss(animated: true, completion: nil)
+    parent?.dismiss(animated: true, completion: nil)
+  }
 
-    func setEvent(newEvent: Event) {
-        event = newEvent
-    }
+  func setEvent(newEvent: Event) {
+    event = newEvent
+  }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
 
-        if let e = event {
-            if #available(iOS 17.0, *) {
-                eventStore.requestWriteOnlyAccessToEvents { granted, error in
-                    DispatchQueue.main.async {
-                        if granted && (error == nil) {
-                            let ek = EKEvent(eventStore: self.eventStore)
-                            ek.title = e.title
-                            ek.startDate = e.beginTimestamp
-                            ek.endDate = e.endTimestamp
-                            ek.timeZone = DateFormatterUtility.shared.timeZone
-                            ek.location = e.location.name
-                            ek.notes = e.description
+    if let e = event {
+      if #available(iOS 17.0, *) {
+        eventStore.requestWriteOnlyAccessToEvents { granted, error in
+          DispatchQueue.main.async {
+            if granted && (error == nil) {
+              let ek = EKEvent(eventStore: self.eventStore)
+              ek.title = e.title
+              ek.startDate = e.beginTimestamp
+              ek.endDate = e.endTimestamp
+              ek.timeZone = DateFormatterUtility.shared.timeZone
+              ek.location = e.location.name
+              ek.notes = e.description
 
-                            let eventController = EKEventEditViewController()
+              let eventController = EKEventEditViewController()
 
-                            eventController.eventStore = self.eventStore
-                            eventController.event = ek
-                            eventController.editViewDelegate = self
-                            eventController.modalPresentationStyle = .overCurrentContext
-                            eventController.modalTransitionStyle = .crossDissolve
+              eventController.eventStore = self.eventStore
+              eventController.event = ek
+              eventController.editViewDelegate = self
+              eventController.modalPresentationStyle = .overCurrentContext
+              eventController.modalTransitionStyle = .crossDissolve
 
-                            self.present(eventController, animated: true, completion: nil)
-                        }
-                    }
-                }
-            } else {
-                eventStore.requestAccess(to: EKEntityType.event, completion: { granted, error in
-                    DispatchQueue.main.async {
-                        if granted, error == nil {
-                            let ek = EKEvent(eventStore: self.eventStore)
-                            ek.title = e.title
-                            ek.startDate = e.beginTimestamp
-                            ek.endDate = e.endTimestamp
-                            ek.timeZone = DateFormatterUtility.shared.timeZone
-                            ek.location = e.location.name
-                            ek.notes = e.description
-
-                            let eventController = EKEventEditViewController()
-
-                            eventController.eventStore = self.eventStore
-                            eventController.event = ek
-                            eventController.editViewDelegate = self
-                            eventController.modalPresentationStyle = .overCurrentContext
-                            eventController.modalTransitionStyle = .crossDissolve
-
-                            self.present(eventController, animated: true, completion: nil)
-                        }
-                    }
-                })
+              self.present(eventController, animated: true, completion: nil)
             }
-
-        } else {
-            print("AddEventController: event is nil")
+          }
         }
+      } else {
+        eventStore.requestAccess(
+          to: EKEntityType.event,
+          completion: { granted, error in
+            DispatchQueue.main.async {
+              if granted, error == nil {
+                let ek = EKEvent(eventStore: self.eventStore)
+                ek.title = e.title
+                ek.startDate = e.beginTimestamp
+                ek.endDate = e.endTimestamp
+                ek.timeZone = DateFormatterUtility.shared.timeZone
+                ek.location = e.location.name
+                ek.notes = e.description
+
+                let eventController = EKEventEditViewController()
+
+                eventController.eventStore = self.eventStore
+                eventController.event = ek
+                eventController.editViewDelegate = self
+                eventController.modalPresentationStyle = .overCurrentContext
+                eventController.modalTransitionStyle = .crossDissolve
+
+                self.present(eventController, animated: true, completion: nil)
+              }
+            }
+          })
+      }
+
+    } else {
+      print("AddEventController: event is nil")
     }
+  }
 }
--- hackertracker/Views/EventsView.swift
+++ hackertracker/Views/EventsView.swift
@@ -7,206 +7,234 @@
 import SwiftUI
 
 struct EventsView: View {
-    let events: [Event]
-    let conference: Conference?
-    let bookmarks: [Int32]
-    @AppStorage("showLocaltime") var showLocaltime: Bool = false
-    @AppStorage("showPastEvents") var showPastEvents: Bool = true
-    @EnvironmentObject var viewModel: InfoViewModel
-    let dfu = DateFormatterUtility.shared
-    var includeNav: Bool = true
-    var navTitle: String = ""
-    @Binding var tappedScheduleTwice: Bool
-    @Binding var schedule: UUID
+  let events: [Event]
+  let conference: Conference?
+  let bookmarks: [Int32]
+  @AppStorage("showLocaltime") var showLocaltime: Bool = false
+  @AppStorage("showPastEvents") var showPastEvents: Bool = true
+  @EnvironmentObject var viewModel: InfoViewModel
+  let dfu = DateFormatterUtility.shared
+  var includeNav: Bool = true
+  var navTitle: String = ""
+  @Binding var tappedScheduleTwice: Bool
+  @Binding var schedule: UUID
 
-    @State private var eventDay = ""
-    @State private var searchText = ""
-    @Binding var filters: Set<Int>
-    @State private var showFilters = false
+  @State private var eventDay = ""
+  @State private var searchText = ""
+  @Binding var filters: Set<Int>
+  @State private var showFilters = false
 
-    var body: some View {
-        if includeNav {
-            NavigationView {
-                EventScrollView(events: events
-                    .filters(typeIds: filters, bookmarks: bookmarks, tagTypes: viewModel.tagtypes)
-                    .search(text: searchText)
-                    .eventDayGroup(), bookmarks: bookmarks, dayTag: eventDay, showPastEvents: showPastEvents, includeNav: includeNav, tappedScheduleTwice: $tappedScheduleTwice, schedule: $schedule)
-                    .navigationTitle(viewModel.conference?.name ?? "Schedule")
-                    .toolbar {
-                        ToolbarItemGroup(placement: .navigationBarLeading) {
-                            Menu {
-                                Toggle(isOn: $showLocaltime) {
-                                    Label("Display Localtime", systemImage: "clock")
-                                }
-                                .onChange(of: showLocaltime) { value in
-                                    print("EventsView: Changing to showLocaltime = \(value)")
-                                    viewModel.showLocaltime = value
-                                    if showLocaltime {
-                                        dfu.update(tz: TimeZone.current)
-                                    } else {
-                                        dfu.update(tz: TimeZone(identifier: conference?.timezone ?? "America/Los_Angeles"))
-                                    }
-                                }
-                                Toggle(isOn: $showPastEvents) {
-                                    Label("Show Past Events", systemImage: "calendar")
-                                }
-                                .onChange(of: showPastEvents) { value in
-                                    print("EventsView: Changing to showPastEvents = \(value)")
-                                    viewModel.showPastEvents = value
-                                }
-                                .toggleStyle(.automatic)
-                            } label: {
-                                Image(systemName: "ellipsis")
-                            }
-                        }
-                        ToolbarItemGroup(placement: .navigationBarTrailing) {
-                            Menu {
-                                Button {
-                                    self.tappedScheduleTwice = true
-                                } label: {
-                                    HStack {
-                                        Text("Top")
-                                        Image(systemName: "chevron.up")
-                                    }
-                                }
-                                ForEach(events.filters(typeIds: filters, bookmarks: bookmarks, tagTypes: viewModel.tagtypes).eventDayGroup(), id: \.key) { day, _ in
-                                    Button(day) {
-                                        eventDay = day
-                                    }
-                                }
+  var body: some View {
+    if includeNav {
+      NavigationView {
+        EventScrollView(
+          events:
+            events
+            .filters(typeIds: filters, bookmarks: bookmarks, tagTypes: viewModel.tagtypes)
+            .search(text: searchText)
+            .eventDayGroup(), bookmarks: bookmarks, dayTag: eventDay,
+          showPastEvents: showPastEvents, includeNav: includeNav,
+          tappedScheduleTwice: $tappedScheduleTwice, schedule: $schedule
+        )
+        .navigationTitle(viewModel.conference?.name ?? "Schedule")
+        .toolbar {
+          ToolbarItemGroup(placement: .navigationBarLeading) {
+            Menu {
+              Toggle(isOn: $showLocaltime) {
+                Label("Display Localtime", systemImage: "clock")
+              }
+              .onChange(of: showLocaltime) { value in
+                print("EventsView: Changing to showLocaltime = \(value)")
+                viewModel.showLocaltime = value
+                if showLocaltime {
+                  dfu.update(tz: TimeZone.current)
+                } else {
+                  dfu.update(
+                    tz: TimeZone(identifier: conference?.timezone ?? "America/Los_Angeles"))
+                }
+              }
+              Toggle(isOn: $showPastEvents) {
+                Label("Show Past Events", systemImage: "calendar")
+              }
+              .onChange(of: showPastEvents) { value in
+                print("EventsView: Changing to showPastEvents = \(value)")
+                viewModel.showPastEvents = value
+              }
+              .toggleStyle(.automatic)
+            } label: {
+              Image(systemName: "ellipsis")
+            }
+          }
+          ToolbarItemGroup(placement: .navigationBarTrailing) {
+            Menu {
+              Button {
+                self.tappedScheduleTwice = true
+              } label: {
+                HStack {
+                  Text("Top")
+                  Image(systemName: "chevron.up")
+                }
+              }
+              ForEach(
+                events.filters(typeIds: filters, bookmarks: bookmarks, tagTypes: viewModel.tagtypes)
+                  .eventDayGroup(), id: \.key
+              ) { day, _ in
+                Button(day) {
+                  eventDay = day
+                }
+              }
 
-                            } label: {
-                                Image(systemName: "chevron.up.chevron.down")
-                            }
+            } label: {
+              Image(systemName: "chevron.up.chevron.down")
+            }
 
-                            Button {
-                                showFilters.toggle()
-                            } label: {
-                                Image(systemName: filters
-                                    .isEmpty
-                                    ? "line.3.horizontal.decrease.circle"
-                                    : "line.3.horizontal.decrease.circle.fill")
-                            }
-                        }
-                    }
-                    .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .automatic))
+            Button {
+              showFilters.toggle()
+            } label: {
+              Image(
+                systemName: filters
+                  .isEmpty
+                  ? "line.3.horizontal.decrease.circle"
+                  : "line.3.horizontal.decrease.circle.fill")
             }
-            .sheet(isPresented: $showFilters) {
-                EventFilters(tagtypes: viewModel.tagtypes.filter { $0.category == "content" && $0.isBrowsable == true }, showFilters: $showFilters, filters: $filters)
-            }
-            .onChange(of: viewModel.conference) { con in
-                print("EventsView.onChange(of: conference) == \(con?.name ?? "not found")")
-                self.filters = []
-            }
-        } else {
-            VStack {
-                EventScrollView(events: events
-                    .filters(typeIds: filters, bookmarks: bookmarks, tagTypes: viewModel.tagtypes)
-                    .search(text: searchText)
-                    .eventDayGroup(), bookmarks: bookmarks, dayTag: eventDay, showPastEvents: showPastEvents, includeNav: includeNav, tappedScheduleTwice: $tappedScheduleTwice, schedule: $schedule)
-                    .navigationTitle(navTitle)
-                    .toolbar {
-                        ToolbarItemGroup(placement: .navigationBarTrailing) {
-                            Menu {
-                                ForEach(events.filters(typeIds: filters, bookmarks: bookmarks, tagTypes: viewModel.tagtypes).eventDayGroup(), id: \.key) { day, _ in
-                                    Button(day) {
-                                        eventDay = day
-                                    }
-                                }
-
-                            } label: {
-                                Image(systemName: "chevron.up.chevron.down")
-                            }
-                        }
-                    }
-                    .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .automatic))
-            }
-            .onChange(of: viewModel.conference) { con in
-                print("EventsView.onChange(of: conference) == \(con?.name ?? "not found")")
-            }
+          }
         }
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .automatic))
+      }
+      .sheet(isPresented: $showFilters) {
+        EventFilters(
+          tagtypes: viewModel.tagtypes.filter {
+            $0.category == "content" && $0.isBrowsable == true
+          }, showFilters: $showFilters, filters: $filters)
+      }
+      .onChange(of: viewModel.conference) { con in
+        print("EventsView.onChange(of: conference) == \(con?.name ?? "not found")")
+        self.filters = []
+      }
+    } else {
+      VStack {
+        EventScrollView(
+          events:
+            events
+            .filters(typeIds: filters, bookmarks: bookmarks, tagTypes: viewModel.tagtypes)
+            .search(text: searchText)
+            .eventDayGroup(), bookmarks: bookmarks, dayTag: eventDay,
+          showPastEvents: showPastEvents, includeNav: includeNav,
+          tappedScheduleTwice: $tappedScheduleTwice, schedule: $schedule
+        )
+        .navigationTitle(navTitle)
+        .toolbar {
+          ToolbarItemGroup(placement: .navigationBarTrailing) {
+            Menu {
+              ForEach(
+                events.filters(typeIds: filters, bookmarks: bookmarks, tagTypes: viewModel.tagtypes)
+                  .eventDayGroup(), id: \.key
+              ) { day, _ in
+                Button(day) {
+                  eventDay = day
+                }
+              }
+
+            } label: {
+              Image(systemName: "chevron.up.chevron.down")
+            }
+          }
+        }
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .automatic))
+      }
+      .onChange(of: viewModel.conference) { con in
+        print("EventsView.onChange(of: conference) == \(con?.name ?? "not found")")
+      }
     }
+  }
 }
 
 struct EventScrollView: View {
-    let events: [(key: String, value: [Event])]
-    let bookmarks: [Int32]
-    let dayTag: String
-    let showPastEvents: Bool
-    let includeNav: Bool
-    let dfu = DateFormatterUtility.shared
-    @Binding var tappedScheduleTwice: Bool
-    @EnvironmentObject var viewModel: InfoViewModel
-    @State var viewShowing = false
-    @Binding var schedule: UUID
+  let events: [(key: String, value: [Event])]
+  let bookmarks: [Int32]
+  let dayTag: String
+  let showPastEvents: Bool
+  let includeNav: Bool
+  let dfu = DateFormatterUtility.shared
+  @Binding var tappedScheduleTwice: Bool
+  @EnvironmentObject var viewModel: InfoViewModel
+  @State var viewShowing = false
+  @Binding var schedule: UUID
 
-    var body: some View {
-        ScrollViewReader { proxy in
-            List(events, id: \.key) { weekday, events in
-                // if showPastEvents || weekday >= Date() {
-                EventData(weekday: weekday, events: events, bookmarks: bookmarks, showPastEvents: showPastEvents)
-                    .id(weekday)
-                // }
-            }
-            .listStyle(.plain)
-            .onChange(of: dayTag) { changedValue in
-                withAnimation {
-                    proxy.scrollTo(changedValue, anchor: .top)
-                }
-            }
-            .onChange(of: tappedScheduleTwice, perform: { tappedTwice in
-                guard tappedTwice else { return }
-                let es = events
-                if viewShowing, includeNav {
-                    if let f = es.first, let e = f.value.first {
-                        let id: String = dfu.dayOfWeekFormatter.string(from: e.beginTimestamp)
-                        withAnimation {
-                            proxy.scrollTo(id, anchor: .top)
-                        }
-                        // schedule = UUID()
-                    }
-                } else {
-                    print("EventScrollView: reset UUID here!")
-                    schedule = UUID()
-                }
-                self.tappedScheduleTwice = false
-            })
-            .onAppear {
-                viewShowing = true
-            }
-            .onDisappear {
-                viewShowing = false
-            }
+  var body: some View {
+    ScrollViewReader { proxy in
+      List(events, id: \.key) { weekday, events in
+        // if showPastEvents || weekday >= Date() {
+        EventData(
+          weekday: weekday, events: events, bookmarks: bookmarks, showPastEvents: showPastEvents
+        )
+        .id(weekday)
+        // }
+      }
+      .listStyle(.plain)
+      .onChange(of: dayTag) { changedValue in
+        withAnimation {
+          proxy.scrollTo(changedValue, anchor: .top)
         }
+      }
+      .onChange(
+        of: tappedScheduleTwice,
+        perform: { tappedTwice in
+          guard tappedTwice else { return }
+          let es = events
+          if viewShowing, includeNav {
+            if let f = es.first, let e = f.value.first {
+              let id: String = dfu.dayOfWeekFormatter.string(from: e.beginTimestamp)
+              withAnimation {
+                proxy.scrollTo(id, anchor: .top)
+              }
+              // schedule = UUID()
+            }
+          } else {
+            print("EventScrollView: reset UUID here!")
+            schedule = UUID()
+          }
+          self.tappedScheduleTwice = false
+        }
+      )
+      .onAppear {
+        viewShowing = true
+      }
+      .onDisappear {
+        viewShowing = false
+      }
     }
+  }
 }
 
 struct EventData: View {
-    let weekday: String
-    let events: [Event]
-    let bookmarks: [Int32]
-    let showPastEvents: Bool
-    let dfu = DateFormatterUtility.shared
+  let weekday: String
+  let events: [Event]
+  let bookmarks: [Int32]
+  let showPastEvents: Bool
+  let dfu = DateFormatterUtility.shared
 
-    var body: some View {
-        Section(header: Text(weekday)) {
-            ForEach(events.eventDayGroup(), id: \.key) { _, timeEvents in
-                // if showPastEvents || time >= Date() {
-                Section {
-                    ForEach(timeEvents.sorted {
-                        $0.beginTimestamp < $1.beginTimestamp
-                    }, id: \.id) { event in
-                        if showPastEvents || event.beginTimestamp >= Date() {
-                            NavigationLink(destination: EventDetailView(eventId: event.id)) {
-                                EventCell(event: event, bookmarks: bookmarks, showDay: false)
-                            }
-                        }
-                    }
-                }
-                .listStyle(.plain)
-                // }
+  var body: some View {
+    Section(header: Text(weekday)) {
+      ForEach(events.eventDayGroup(), id: \.key) { _, timeEvents in
+        // if showPastEvents || time >= Date() {
+        Section {
+          ForEach(
+            timeEvents.sorted {
+              $0.beginTimestamp < $1.beginTimestamp
+            }, id: \.id
+          ) { event in
+            if showPastEvents || event.beginTimestamp >= Date() {
+              NavigationLink(destination: EventDetailView(eventId: event.id)) {
+                EventCell(event: event, bookmarks: bookmarks, showDay: false)
+              }
             }
+          }
         }
-        .headerProminence(.increased)
+        .listStyle(.plain)
+        // }
+      }
     }
+    .headerProminence(.increased)
+  }
 }
